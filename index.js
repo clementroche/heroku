2c57984a7ebe666b027e836daf245d0f736ef7b2
Clément Roche <rchclement@gmail.com>
2019-02-28 17:21:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const port = process.env.PORT || 3000
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -14,7 +15,7 @@ io.on('connection', (socket) => {
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
-http.listen(80, () => {
+http.listen(port, () => {
     console.log('listening on *:80');
 });
 
