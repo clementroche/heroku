a418d11445c065afdd49b1602406566cfd494df2
rocheclement <rchclement@gmail.com>
2019-04-23 00:14:57 +0200

    create room

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const io = require('socket.io')(http);
 
 const port = process.env.PORT || 3000
 
+function createID() {
+  return Math.random().toString(36).substr(2, 9)
+}
+
 io.on('connection', (socket) => {
     console.log('connected', socket.id)
     console.log(socket.handshake.query.device)
+    if(socket.handshake.query.device === 'desktop') {
+        let id = createID()
+        socket.join(id)
+        io.to(socket.id).emit('room created',id)
+    } else if(socket.handshake.query.device === 'mobile') {
+        let id = socket.handshake.query.roomID
+        socket.join(id)
+        io.to(socket.id).emit('room joined',id)
+    }
 
     socket.on('disconnect', () => {
         console.log('Client disconnected', socket.id)
